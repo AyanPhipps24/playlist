6fd43b57dddb33de5fee3a700dc3321565c5b083
Ayan Phipps <atphipps24@gmail.com>
2019-02-04 23:13:42 +0000

    playlist

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["Twerk","Startender","Thotiana"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
-
-
+var images_links =["https://static.spin.com/files/2019/01/city-girls-card-b-twerk-video-1547671528-640x431.jpg",
+"https://cps-static.rovicorp.com/3/JPG_400/MI0004/625/MI0004625372.jpg?partner=allrovi.com"
+,"https://pbs.twimg.com/profile_images/1049931791899844608/esnDJeH9_400x400.jpg"]
+var  artists = ["City Girls","A boogie wit a hoodie","Blueface"];
+var songlengths =["2:47","3:13","2:10"];
+var 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
 
