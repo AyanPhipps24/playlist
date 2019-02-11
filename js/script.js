e53f37ef6dc509069e8393fe88aa673ad45618fa
Ayan Phipps <atphipps24@gmail.com>
2019-02-11 23:19:59 +0000

    Playlist

--- js/script.js
+++ js/script.js
@@ -9,11 +9,28 @@ var images_links =["https://static.spin.com/files/2019/01/city-girls-card-b-twer
 ,"https://pbs.twimg.com/profile_images/1049931791899844608/esnDJeH9_400x400.jpg"]
 var  artists = ["City Girls","A boogie wit a hoodie","Blueface"];
 var songlengths =["2:47","3:13","2:10"];
-var 
+var songLinks = [
+"https://www.youtube.com/watch?v=QryoOF5jEbc",
+"https://www.youtube.com/watch?v=DEoAbqwmZro"
+,"https://www.youtube.com/watch?v=v_qFfgTYdSo"]
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
+songs.forEach(function(songs){
+    $('#songs').append("<p>" + songs + "</p>")
+});
 }
+images_links.forEach(function(images_links){
+   $('#images_links').append("<p>" +images_links+ "</p>")
+});
+artists.forEach(function(artists){
+   $('#artists').append("<p>" +artists+ "</p>")
+});
+songlengths.forEach(function(songlengths){
+   $('#songlengths').append("<p>" + songlengths +"</p>")
+});
+songLinks.forEach(function(songLinks){
+   $('#songLinks').append("<a href='" +songLinks+"'>")
+});
 
 function emptySongInfo(){
     $("#songs").empty();
